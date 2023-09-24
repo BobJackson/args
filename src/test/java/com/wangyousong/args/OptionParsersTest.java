dcd82821e27fe11be227a2b858ba30effabbb464
Bob Jackson <wangyousong@9191offer.com>
2023-09-24 17:58:38 +0800

    refactor: make BooleanOptionParserTest inner class of OptionParsersTest; and rename to BooleanOptionParser

--- src/test/java/com/wangyousong/args/OptionParsersTest.java
+++ src/test/java/com/wangyousong/args/OptionParsersTest.java
@@ -2,57 +2,102 @@ package com.wangyousong.args;
 
 import com.wangyousong.args.exception.InsufficientArgumentsException;
 import com.wangyousong.args.exception.TooManyArgumentsException;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.lang.annotation.Annotation;
 import java.util.List;
 import java.util.function.Function;
 
-import static com.wangyousong.args.BooleanOptionParserTest.option;
+import static com.wangyousong.args.OptionParsersTest.BooleanOptionParser.option;
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OptionParsersTest {
 
-    // sad path
-    @Test
-    void should_not_accept_extra_argument_for_single_value_option() {
-        TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
-            (OptionParsers.unary(0, Integer::parseInt)).parse(asList("-p", "8080", "8081"), option("p"));
-        });
+    @Nested
+    class UnaryOptionParser {
+        // sad path
+        @Test
+        void should_not_accept_extra_argument_for_single_value_option() {
+            TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
+                (OptionParsers.unary(0, Integer::parseInt)).parse(asList("-p", "8080", "8081"), option("p"));
+            });
 
-        assertEquals("p", e.getOption());
+            assertEquals("p", e.getOption());
+        }
+
+        // sad path
+        @ParameterizedTest
+        @ValueSource(strings = {"-p -l", "-p"})
+        void should_not_accept_insufficient_argument_for_single_value_option(String arguments) {
+            InsufficientArgumentsException e = assertThrows(InsufficientArgumentsException.class, () -> {
+                (OptionParsers.unary(0, Integer::parseInt)).parse(List.of(arguments.split(" ")), option("p"));
+            });
+
+            assertEquals("p", e.getOption());
+        }
+
+        // default value
+        @Test
+        void should_set_default_value_to_0_for_int_option() {
+            Function<String, Object> whatever = (it) -> null;
+            Object defaultValue = new Object();
+
+            assertSame(defaultValue, OptionParsers.unary(defaultValue, whatever).parse(List.of(), option("p")));
+        }
+
+
+        // happy path
+        @Test
+        void should_parse_value_if_flag_present() {
+            Object parsed = new Object();
+            Function<String, Object> parse = (it) -> parsed;
+            Object whatever = new Object();
+
+            assertSame(parsed, OptionParsers.unary(whatever, parse).parse(List.of("-p", "8080"), option("p")));
+        }
     }
 
-    // sad path
-    @ParameterizedTest
-    @ValueSource(strings = {"-p -l", "-p"})
-    void should_not_accept_insufficient_argument_for_single_value_option(String arguments) {
-        InsufficientArgumentsException e = assertThrows(InsufficientArgumentsException.class, () -> {
-            (OptionParsers.unary(0, Integer::parseInt)).parse(List.of(arguments.split(" ")), option("p"));
-        });
+    @Nested
+    class BooleanOptionParser {
 
-        assertEquals("p", e.getOption());
-    }
+        @Test
+        void should_not_accept_extra_argument_for_boolean_option() {
+            TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
+                OptionParsers.bool().parse(asList("-l", "t"), option("l"));
+            });
 
-    // default value
-    @Test
-    void should_set_default_value_to_0_for_int_option() {
-        Function<String, Object> whatever = (it) -> null;
-        Object defaultValue = new Object();
+            assertEquals("l", e.getOption());
+        }
 
-        assertSame(defaultValue, OptionParsers.unary(defaultValue, whatever).parse(List.of(), option("p")));
-    }
+        // Default Value
+        @Test
+        void should_set_default_value_to_false_if_option_not_present() {
+            assertFalse(OptionParsers.bool().parse(List.of(), option("l")));
+        }
+
+        // Happy Path
+        @Test
+        void should_set_boolean_option_to_true_if_flag_present() {
+            assertTrue(OptionParsers.bool().parse(List.of("-l"), option("l")));
+        }
 
 
-    // happy path
-    @Test
-    void should_parse_value_if_flag_present() {
-        Object parsed = new Object();
-        Function<String, Object> parse = (it) -> parsed;
-        Object whatever = new Object();
+        static Option option(String value) {
+            return new Option() {
+                @Override
+                public Class<? extends Annotation> annotationType() {
+                    return Option.class;
+                }
 
-        assertSame(parsed, OptionParsers.unary(whatever, parse).parse(List.of("-p", "8080"), option("p")));
+                @Override
+                public String value() {
+                    return value;
+                }
+            };
+        }
     }
 }
