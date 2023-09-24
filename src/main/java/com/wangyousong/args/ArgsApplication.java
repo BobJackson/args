0b0835ad369dd5f8f1fbddfbd75d526db9c728b9
Bob Jackson <wangyousong@9191offer.com>
2023-09-24 11:23:01 +0800

    init project

---
+++ src/main/java/com/wangyousong/args/ArgsApplication.java
@@ -0,0 +1,13 @@
+package com.wangyousong.args;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangyousong/args/ArgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangyousong.args;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
