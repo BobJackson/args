ca5a924b17a9dbc39089d4572a00f5e09f0af613
Bob Jackson <wangyousong@9191offer.com>
2023-09-24 17:11:42 +0800

    refactor: parse() to improve code readability

--- src/main/java/com/wangyousong/args/SingleValueOptionParser.java
+++ src/main/java/com/wangyousong/args/SingleValueOptionParser.java
@@ -5,6 +5,7 @@ import com.wangyousong.args.exception.TooManyArgumentsException;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 class SingleValueOptionParser<T> implements OptionParser<T> {
 
@@ -20,11 +21,19 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         if (index == -1) return defaultValue;
-        if (index + 1 == arguments.size() ||
-                arguments.get(index + 1).startsWith("-")) throw new InsufficientArgumentsException(option.value());
-        if (index + 2 < arguments.size() &&
-                !arguments.get(index + 2).startsWith("-")) throw new TooManyArgumentsException(option.value());
+        List<String> values = values(arguments, index);
+        if (values.size() < 1) throw new InsufficientArgumentsException(option.value());
+        if (values.size() > 1) throw new TooManyArgumentsException(option.value());
         return valueParser.apply(arguments.get(index + 1));
+    }
+
+    private static List<String> values(List<String> arguments, int index) {
+        int followingFlag = IntStream.range(index + 1, arguments.size())
+                .filter(it -> arguments.get(it).startsWith("-"))
+                .findFirst()
+                .orElse(arguments.size());
+
+        return arguments.subList(index + 1, followingFlag);
     }
 
 }
