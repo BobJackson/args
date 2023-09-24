9ad91bd62d2926f2aabdc6f76e77cdf8b8632216
Bob Jackson <wangyousong@9191offer.com>
2023-09-24 12:37:49 +0800

    refactor: getOptionParser(), 同时准备使用PARSERS消除多个if分支的判断

--- src/main/java/com/wangyousong/args/Args.java
+++ src/main/java/com/wangyousong/args/Args.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Args {
     public static <T> T parse(Class<T> optionsClass, String... args) {
@@ -20,27 +21,31 @@ public class Args {
     }
 
     private static Object parseOption(List<String> arguments, Parameter parameter) {
-        Object value = null;
-        Option option = parameter.getAnnotation(Option.class);
-
-        if (parameter.getType() == boolean.class) {
-            value = parseBoolean(arguments, option);
-        }
-        if (parameter.getType() == int.class) {
-            value = parseInt(arguments, option);
-        }
-        if (parameter.getType() == String.class) {
-            value = parseString(arguments, option);
-        }
-        return value;
+        return getOptionParser(parameter.getType()).parse(arguments, parameter.getAnnotation(Option.class));
     }
 
-    interface  OptionParser {
+    private static Map<Class<?>, OptionParser> PARSERS = Map.of(
+            boolean.class, new BooleanOptionParser(),
+            int.class, new IntOptionParser(),
+            String.class, new StringOptionParser()
+    );
+
+    private static OptionParser getOptionParser(Class<?> type) {
+        OptionParser parser = null;
+        if (type == boolean.class) {
+            parser = new BooleanOptionParser();
+        }
+        if (type == int.class) {
+            parser = new IntOptionParser();
+        }
+        if (type == String.class) {
+            parser = new StringOptionParser();
+        }
+        return parser;
+    }
+
+    interface OptionParser {
         Object parse(List<String> arguments, Option option);
-    }
-
-    private static Object parseString(List<String> arguments, Option option) {
-        return new StringOptionParser().parse(arguments, option);
     }
 
     static class StringOptionParser implements OptionParser {
@@ -50,19 +55,11 @@ public class Args {
         }
     }
 
-    private static Object parseInt(List<String> arguments, Option option) {
-        return new IntOptionParser().parse(arguments, option);
-    }
-
     static class IntOptionParser implements OptionParser {
         @Override
         public Object parse(List<String> arguments, Option option) {
             return Integer.parseInt(arguments.get(arguments.indexOf("-" + option.value()) + 1));
         }
-    }
-
-    private static Object parseBoolean(List<String> arguments, Option option) {
-        return new BooleanOptionParser().parse(arguments, option);
     }
 
     static class BooleanOptionParser implements OptionParser {
