0bdd4fb3d0cae817c578187914bcb0c9123175e0
Bob Jackson <wangyousong@9191offer.com>
2023-09-24 14:58:58 +0800

    消除明显的bug,对于直接抛NPE改为更友好的自定义的异常

--- src/test/java/com/wangyousong/args/ArgsTest.java
+++ src/test/java/com/wangyousong/args/ArgsTest.java
@@ -19,6 +19,17 @@ class ArgsTest {
     }
 
     @Test
+    void should_throw_illegal_option_exception_if_annotation_not_present() {
+        IllegalOptionException e = assertThrows(IllegalOptionException.class,
+                () -> Args.parse(OptionsWithoutAnnotation.class, "-l", "-p", "8080", "-d", "/usr/logs"));
+
+        assertEquals("port", e.getParameter());
+    }
+
+    record OptionsWithoutAnnotation(@Option("l") boolean logging, int port, @Option("d") String directory) {
+    }
+
+    @Test
     @Disabled
     void should_example_2() {
         ListOptions options = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
